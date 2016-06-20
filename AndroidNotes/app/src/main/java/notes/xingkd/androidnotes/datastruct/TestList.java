cbd125d79419f6fd19ac4fa62095c452daded537
xingkongdao <xingkongdao@syberos.com>
2016-06-20 12:38:14 +0800

    add testAdapter testFragment testList
    add testAdapter testFragment testList

---
+++ AndroidNotes/app/src/main/java/notes/xingkd/androidnotes/datastruct/TestList.java
@@ -0,0 +1,7 @@
+package notes.xingkd.androidnotes.datastruct;
+
+/**
+ * Created by xkd on 16-6-20.
+ */
+public class TestList {
+}
---
+++ AndroidNotes/app/src/main/java/notes/xingkd/androidnotes/fragment/TestFragment.java
@@ -0,0 +1,7 @@
+package notes.xingkd.androidnotes.fragment;
+
+/**
+ * Created by xkd on 16-6-20.
+ */
+public class TestFragment {
+}
